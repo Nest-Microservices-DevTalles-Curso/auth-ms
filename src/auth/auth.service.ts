71b1d99c5e91a8591b3ab54b91b6596bbda77649
Kevin Espinoza <kevinespinozadev@gmail.com>
2024-10-29 20:26:44 -0300

    register user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,8 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
 import { PrismaClient } from '@prisma/client';
+import { RegisterUserDto } from './dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService extends PrismaClient implements OnModuleInit {
@@ -9,6 +12,48 @@ export class AuthService extends PrismaClient implements OnModuleInit {
     onModuleInit() {
         this.$connect();
         this.logger.log('MongoDB connected');
+    }
+
+    async registerUser(registerUserDto: RegisterUserDto) {
+
+        const {name, email, password} = registerUserDto;
+
+        try {
+
+            const user = await this.user.findUnique({
+                where: {
+                    email
+                }
+            });
+
+            if (user) {
+                throw new RpcException({
+                    status: 400,
+                    message: 'User already exists'
+                });
+            }
+
+            const newUser = await this.user.create({
+                data: {
+                    email: email,
+                    password: bcrypt.hashSync(password, 10),
+                    name: name
+                }
+            });
+
+            const {password: __, ...restWithoutPassword} = newUser;
+
+            return {
+                user: restWithoutPassword,
+                token: 'ABC'
+            }
+            
+        } catch (error) {
+            throw new RpcException({
+                status: 400,
+                message: error.message
+            });
+        }
     }
 
 }
